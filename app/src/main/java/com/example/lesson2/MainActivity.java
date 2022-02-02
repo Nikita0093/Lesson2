caaa00d4a872858871206422418992f9eabab6b9
Nikita Dyachenko <nikita.dyachenko93@bk.ru>
2022-02-03 00:50:14 +0300

    Добавили логику цифровыым кнопкам

--- app/src/main/java/com/example/lesson2/MainActivity.java
+++ app/src/main/java/com/example/lesson2/MainActivity.java
@@ -5,38 +5,138 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Switch;
 import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
     int DisplayCounter = 0;
+    TextView DisplayInfo;
+    Button buttonBackspace;
+    Button buttonOne;
+    Button buttonTwo;
+    Button buttonHooks;
+    Button buttonPercent;
+    Button buttonDelete;
+    Button buttonSeven;
+    Button buttonEight;
+    Button buttonNine;
+    Button buttonMultiplication;
+    Button buttonFour;
+    Button buttonFive;
+    Button buttonSix;
+    Button buttonSubtraction;
+    Button buttonThree;
+    Button buttonPlus;
+    Button buttonPlus_minus;
+    Button buttonZero;
+    Button buttonDot;
+    Button buttonEqual;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        TextView DisplayInfo = findViewById(R.id.DisplayInfo);
-
-
-        Button buttonBackspace = findViewById(R.id.buttonBackspace);
-        buttonBackspace.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                DisplayInfo.setText(null);
-            }
-        });
-
-        Button buttonOne = findViewById(R.id.buttonOne);
-        buttonOne.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                DisplayCounter = 1 ;
-                DisplayInfo.setText(String.format(DisplayCounter + ""));
-
-            }
-        });
-
+        Initialization();
+        SetListener();
 
 
     }
+
+    private void SetListener() {
+        buttonZero.setOnClickListener(this);
+        buttonOne.setOnClickListener(this);
+        buttonTwo.setOnClickListener(this);
+        buttonThree.setOnClickListener(this);
+        buttonFour.setOnClickListener(this);
+        buttonFive.setOnClickListener(this);
+        buttonSix.setOnClickListener(this);
+        buttonSeven.setOnClickListener(this);
+        buttonEight.setOnClickListener(this);
+        buttonNine.setOnClickListener(this);
+        buttonDot.setOnClickListener(this);
+        buttonBackspace.setOnClickListener(this);
+    }
+
+
+    public void Initialization() {
+        DisplayInfo = findViewById(R.id.DisplayInfo);
+        buttonBackspace = findViewById(R.id.buttonBackspace);
+        buttonHooks = findViewById(R.id.buttonHooks);
+        buttonPercent = findViewById(R.id.buttonPercent);
+        buttonDelete = findViewById(R.id.buttonDelete);
+        buttonSeven = findViewById(R.id.buttonSeven);
+        buttonEight = findViewById(R.id.buttonEight);
+        buttonNine = findViewById(R.id.buttonNine);
+        buttonMultiplication = findViewById(R.id.buttonMultiplication);
+        buttonFour = findViewById(R.id.buttonFour);
+        buttonFive = findViewById(R.id.buttonFive);
+        buttonSix = findViewById(R.id.buttonSix);
+        buttonSubtraction = findViewById(R.id.buttonSubtraction);
+        buttonOne = findViewById(R.id.buttonOne);
+        buttonTwo = findViewById(R.id.buttonTwo);
+        buttonThree = findViewById(R.id.buttonThree);
+        buttonPlus = findViewById(R.id.buttonPlus);
+        buttonPlus_minus = findViewById(R.id.buttonPlus_minus);
+        buttonZero = findViewById(R.id.buttonZero);
+        buttonDot = findViewById(R.id.buttonDot);
+        buttonEqual = findViewById(R.id.buttonEqual);
+    }
+
+    @Override
+    public void onClick(View view) {
+        switch (view.getId()) {
+            case (R.id.buttonZero): {
+                DisplayInfo.append("0");
+                break;
+            }
+            case (R.id.buttonOne): {
+                DisplayInfo.append("1");
+                break;
+            }
+            case (R.id.buttonTwo): {
+                DisplayInfo.append("2");
+                break;
+            }
+            case (R.id.buttonThree): {
+                DisplayInfo.append("3");
+                break;
+            }
+            case (R.id.buttonFour): {
+                DisplayInfo.append("4");
+                break;
+            }
+            case (R.id.buttonFive): {
+                DisplayInfo.append("5");
+                break;
+            }
+            case (R.id.buttonSix): {
+                DisplayInfo.append("6");
+                break;
+            }
+            case (R.id.buttonSeven): {
+                DisplayInfo.append("7");
+                break;
+            }
+            case (R.id.buttonEight): {
+                DisplayInfo.append("8");
+                break;
+            }
+            case (R.id.buttonNine): {
+                DisplayInfo.append("9");
+                break;
+            }
+            case (R.id.buttonDot): {
+                DisplayInfo.append(".");
+                break;
+            }
+            case (R.id.buttonBackspace): {
+                DisplayInfo.setText(null);
+                break;
+            }
+
+        }
+    }
 }
+
+
