53de3092a291f1d955592ebfa59a95339b56d541
Nikita Dyachenko <nikita.dyachenko93@bk.ru>
2022-02-02 15:57:49 +0300

    Добавили экран и начали прописывать логику

--- app/src/main/java/com/example/lesson2/MainActivity.java
+++ app/src/main/java/com/example/lesson2/MainActivity.java
@@ -3,12 +3,40 @@ package com.example.lesson2;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+    int DisplayCounter = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        TextView DisplayInfo = findViewById(R.id.DisplayInfo);
+
+
+        Button buttonBackspace = findViewById(R.id.buttonBackspace);
+        buttonBackspace.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                DisplayInfo.setText(null);
+            }
+        });
+
+        Button buttonOne = findViewById(R.id.buttonOne);
+        buttonOne.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                DisplayCounter = 1 ;
+                DisplayInfo.setText(String.format(DisplayCounter + ""));
+
+            }
+        });
+
+
+
     }
 }
