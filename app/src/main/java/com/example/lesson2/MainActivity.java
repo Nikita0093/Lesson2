880d0948852523d587543c04fd074fefd49d9ff9
Nikita Dyachenko <nikita.dyachenko93@bk.ru>
2022-02-08 21:32:39 +0300

    Урок 4 - Добавили иконку и собрали APK

--- app/src/main/java/com/example/lesson2/MainActivity.java
+++ app/src/main/java/com/example/lesson2/MainActivity.java
@@ -2,7 +2,6 @@ package com.example.lesson2;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
